12467c72c72587d0fa1b93e731336361144881ac
Josevaldo Sicuba <josevaldosicuba@gmail.com>
2024-04-17 13:11:57 +0100

    up structure not animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        "nike":'url("/src/assets/bg.png")',
+      }
+    },
   },
   plugins: [],
 }

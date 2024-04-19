e483becf90f96c4ab688c157690a2bc5ee56bda9
Josevaldo Sicuba <josevaldosicuba@gmail.com>
2024-04-19 13:45:28 +0100

    left side

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily:{
+        "sans":["Poppins"]
+      },
       backgroundImage:{
         "nike":'url("/src/assets/bg.png")',
       }

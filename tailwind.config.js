720f0911138ce0b78ed1715d719d09caee72073f
Josevaldo Sicuba <josevaldosicuba@gmail.com>
2024-04-22 23:44:25 +0100

    up done project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,22 @@ module.exports = {
       },
       backgroundImage:{
         "nike":'url("/src/assets/bg.png")',
+      },
+      keyframes:{
+        floatRight:{
+          "0%": { transform: "translateX(0px)"},
+          "50%": { transform: "translateX(8px)"},
+          "100%": { transform: "translateX(0px)"},
+        },
+        opacityPulse:{
+          "0%":{opacity: 1},
+          "50%":{opacity: 0.2},
+          "100%":{opacity: 1},
+        }
+      },
+      animation:{
+        floatRight: "floatRight 2.5s infinite",
+        opacityPulse: "opacityPulse 2.5s infinite",
       }
     },
   },
